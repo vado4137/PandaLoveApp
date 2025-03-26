a98c86074cab26530980f76a2bfc9455f716e360
4137-Banger <joda80@outlook.de>
2025-03-26 22:57:59 +0100

    First commit

---
+++ src/screens/FeelingsScreen.js
@@ -0,0 +1,4 @@
+import { View, Text } from 'react-native';
+export default function FeelingsScreen() {
+  return <View><Text>Feelings Tab</Text></View>;
+}
---
+++ src/screens/GalleryScreen.js
@@ -0,0 +1,4 @@
+import { View, Text } from 'react-native';
+export default function GalleryScreen() {
+  return <View><Text>Galerie Tab</Text></View>;
+}
